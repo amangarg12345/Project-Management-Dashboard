a065168f8b0607b5a0f641b3ecaf74f86d1d83ca
Aman Garg <amangarg24071995@gmail.com>
2024-11-15 10:47:49 +0530

    Add more task in slice hardcore

--- src/utils/taskSlice.js
+++ src/utils/taskSlice.js
@@ -7,7 +7,19 @@ const taskSlice = createSlice({
             {projectId : '1', taskId : '3', taskName :"Task12" , taskDescription :"Task Description"},
             {projectId : '2' , taskId : '4',taskName :"Task22" , taskDescription :"Task Description"},
             {projectId : '1', taskId : '5',taskName :"Task13" , taskDescription :"Task Description"},
-            {projectId : '2' ,taskId : '6',taskName :"Task23" , taskDescription :"Task Description"}
+            {projectId : '2' ,taskId : '6',taskName :"Task23" , taskDescription :"Task Description"},
+            {projectId : '1', taskId : '7', taskName :"Task14" , taskDescription :"Task Description"},
+            {projectId : '2' ,taskId : '8',taskName :"Task24" , taskDescription :"Task Description"},
+            {projectId : '1', taskId : '9', taskName :"Task15" , taskDescription :"Task Description"},
+            {projectId : '2' , taskId : '10',taskName :"Task25" , taskDescription :"Task Description"},
+            {projectId : '1', taskId : '11',taskName :"Task16" , taskDescription :"Task Description"},
+            {projectId : '2' ,taskId : '12',taskName :"Task26" , taskDescription :"Task Description"},
+            {projectId : '1', taskId : '13', taskName :"Task17" , taskDescription :"Task Description"},
+            {projectId : '2' ,taskId : '14',taskName :"Task27" , taskDescription :"Task Description"},
+            {projectId : '1', taskId : '15', taskName :"Task18" , taskDescription :"Task Description"},
+            {projectId : '2' , taskId : '16',taskName :"Task28" , taskDescription :"Task Description"},
+            {projectId : '1', taskId : '17',taskName :"Task19" , taskDescription :"Task Description"},
+            {projectId : '2' ,taskId : '18',taskName :"Task29" , taskDescription :"Task Description"}
         ]
     },
     reducers : {
